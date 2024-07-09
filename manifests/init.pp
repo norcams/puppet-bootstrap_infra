86db0bd1fcbdd8bf590b7e508e47911f654ab64d
Raymond Kristiansen <raymond.kristiansen@uib.no>
2024-07-09 13:52:44 +0200

    refactor: moved default values to common.yaml

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,14 +2,14 @@
 class bootstrap_infra(
   String $domain,
   String $mirror,
-  String $rootpw            = '$1$de0ytuAp$9wuB2AJxYQrEM9Hxa4Ihp/',
-  String $timezone          = 'Europe/Oslo',
-  String $keyboard          = 'no',
-  String $nameserver        = '8.8.8.8',
-  String $puppetrepo        = 'https://github.com/norcams/himlar',
-  String $epelrepo          = 'https://download.iaas.uio.no/nrec/prod/el8/epel',
-  String $interface         = 'eth1',
-  Hash $kickstart_defaults  = {}
+  String $rootpw,
+  String $timezone,
+  String $keyboard,
+  String $nameserver,
+  String $puppetrepo,
+  String $epelrepo,
+  String $interface,
+  Hash $kickstart_defaults,
 ) {
 
 }
